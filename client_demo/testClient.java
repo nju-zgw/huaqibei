b68c6c3e6531ba6cf6d06e1c9968c7c8e431bda0
nju-zgw <zgw13@software.nju.edu.cn>
2015-08-10 00:54:55 +0800

    client demo

---
+++ client_demo/testClient.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Created by zgw on 2015/7/29.
+ */
+public class testClient {
+    public static String loadJson (String url) {
+        StringBuilder json = new StringBuilder();
+        try {
+            URL urlObject = new URL(url);
+            URLConnection uc = urlObject.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(uc.getInputStream()));
+            String inputLine = null;
+            while ( (inputLine = in.readLine()) != null) {
+                json.append(inputLine);
+            }
+            in.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return json.toString();
+    }
+    public static void main(String [] args){
+        System.out.println(loadJson("http://localhost:8080/data/person.json?id=18"));
+    }
+}
